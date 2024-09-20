b632453fa5261b9ebfa718133113c04723570a16
Igor Todorovski <itodorov@ca.ibm.com>
2024-09-20 17:20:51 -0400

    Change organization to zopencommunity

--- cicd.groovy
+++ cicd.groovy
@@ -6,10 +6,10 @@ node('linux')
                         branches: [[name: '*/main']],
                         doGenerateSubmoduleConfigurations: false,
                         extensions: [],
-                        userRemoteConfigs: [[url: 'https://github.com/ZOSOpenTools/sshpassport.git']]])
+                        userRemoteConfigs: [[url: 'https://github.com/zopencommunity/sshpassport.git']]])
         }
 
         stage('Build') {
-                build job: 'Port-Pipeline', parameters: [string(name: 'PORT_GITHUB_REPO', value: 'https://github.com/ZOSOpenTools/sshpassport.git'), string(name: 'PORT_DESCRIPTION', value: 'A non-interactive ssh password provider.' )]
+                build job: 'Port-Pipeline', parameters: [string(name: 'PORT_GITHUB_REPO', value: 'https://github.com/zopencommunity/sshpassport.git'), string(name: 'PORT_DESCRIPTION', value: 'A non-interactive ssh password provider.' )]
         }
 }
